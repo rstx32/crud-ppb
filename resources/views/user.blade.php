b420e427362a860f0d1fd31a6e478d740718cfaf
rstx32 <rstx32@gmail.com>
2020-01-09 22:43:52 +0700

    tambah view user index

---
+++ resources/views/user.blade.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet">
+        <title>User</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="card mt-5">
+                <div class="card-header text-center">
+                    CRUD User
+                </div>
+                <div class="card-body">
+                    <a href="/user/tambah" class="btn btn-primary">Input User Baru</a>
+                    <a href="/user/cetak_pdf" class="btn btn-primary" target="_blank">CETAK PDF</a>
+                    <br/>
+                    <table class="table table-bordered table-hover table-striped">
+                        <thead>
+                            <tr>
+                                <th>Username</th>
+                                <th>Password</th>
+                                <th>Email</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($user as $p)
+                            <tr>
+                                <td>{{ $p->username }}</td>
+                                <td>{{ $p->password }}</td>
+                                <td>{{ $p->email }}</td>
+                                <td>
+                                    <a href="/user/edit/{{ $p->username }}" class="btn btn-warning">Edit</a>
+                                    <a href="/user/hapus/{{ $p->username }}" class="btn btn-danger">Hapus</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
