3fd355f79b67c2cde8caaada43c9e5c3b4f195c9
rstx32 <rstx32@gmail.com>
2020-01-09 23:43:20 +0700

    edit pakai foreach

---
+++ resources/views/user_edit.blade.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet">
+        <title>Edit User</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="card mt-5">
+                <div class="card-header text-center">
+                    Edit User
+                </div>
+                <div class="card-body">
+                    <a href="/user" class="btn btn-primary">Kembali</a>
+                    <br/>
+                    <br/>
+                    @foreach($user as $p)
+                    <form method="post" action="/user/update/{{$p->username}}">
+
+                        {{ csrf_field() }}
+                        {{ method_field('PUT') }}                        
+
+                        <div class="form-group">
+                            <label>Username</label>
+                            <input type="text" name="username" class="form-control" placeholder="Username..." value=" {{ $p->username }}">
+
+                            @if($errors->has('username'))
+                                <div class="text-danger">
+                                    {{ $errors->first('username')}}
+                                </div>
+                            @endif
+
+                        </div>
+
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="text" name="password" class="form-control" placeholder="Password..." value=" {{ $p->password }}">
+
+                            @if($errors->has('password'))
+                                <div class="text-danger">
+                                    {{ $errors->first('password')}}
+                                </div>
+                            @endif
+
+                        </div>
+
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input type="text" name="email" class="form-control" placeholder="Email..." value=" {{ $p->email }}">
+
+                            @if($errors->has('email'))
+                                <div class="text-danger">
+                                    {{ $errors->first('email')}}
+                                </div>
+                            @endif
+
+                        </div>
+
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-success" value="Simpan">
+                        </div>
+
+                    </form>
+                    @endforeach
+
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
