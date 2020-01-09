babefc4f31cb78cffc04e7bfe3d320480035c11d
rstx32 <rstx32@gmail.com>
2020-01-09 22:43:31 +0700

    tambah model user

--- app/User.php
+++ app/User.php
@@ -8,23 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    protected $table = "users";
+    protected $primaryKey = 'username';
+    protected $fillable = ['username','password','email'];
 }
