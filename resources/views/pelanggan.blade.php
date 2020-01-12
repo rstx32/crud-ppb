eb17d507fd6c6ae1c23c708f1dafa768aca3352a
rstx32 <rstx32@gmail.com>
2020-01-13 00:39:34 +0700

    fix pelanggan view

--- resources/views/pelanggan.blade.php
+++ resources/views/pelanggan.blade.php
@@ -1,12 +1,46 @@
-@extends('admin.admin')
-    @section('content')
-    <body>
-        <div class="container">
+@extends('admin.master')
+@section('content')
+<!-- Sidebar -->
+<div class="sidebar">
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item has-treeview menu-open">
+              <li class="nav-item">
+                <a href="./dashboard" class="nav-link">
+                <i class="nav-icon fas fa-tachometer-alt"></i>
+                  <p>Dashboard</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="./barang" class="nav-link">
+                <i class="nav-icon fas fa-th"></i>
+                  <p>Barang</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="./pelanggan" class="nav-link active">
+                <i class="nav-icon fas fa-table"></i>
+                  <p>Pelanggan</p>
+                </a>
+              </li>
+          </li>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+  
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Main content -->
+    <section class="content">
+    <div class="container">
             <div class="card mt-5">
-                <div class="card-header">
-                    Daftar Pelanggan
-                </div>
                 <div class="card-body">
+                    <h1 class="text-center text-dark">Daftar Pelanggan</h1>
                     <a href="/pelanggan/tambah" class="btn btn-primary">Input Pelanggan Baru</a>
                     <a href="/pelanggan/cetak_pdf" class="btn btn-danger" target="_blank">CETAK PDF</a>
                     <a href="/pelanggan/export_excel" class="btn btn-success my-3" target="_blank">EXPORT EXCEL</a>
@@ -37,5 +71,6 @@
                 </div>
             </div>
         </div>
-    </body>
-    @endsection
+    </section>
+</div>
+@endsection
