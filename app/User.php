403ed03135a12834e2316050104755796b618722
rstx32 <rstx32@gmail.com>
2020-01-12 16:48:25 +0700

    change to default User model

--- app/User.php
+++ app/User.php
@@ -3,12 +3,27 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    protected $table = "users";
-    protected $primaryKey = 'email';
-    protected $fillable = ['username','password','email'];
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 }
