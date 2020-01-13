968fad34aca83c619ddf3426e93f2b153486e5ce
rstx32 <rstx32@gmail.com>
2020-01-13 08:02:05 +0700

    change login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,102 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Login Admin PPB</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="{{ asset('loginTemplate/images/icons/favicon.ico') }}"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('loginTemplate/vendor/bootstrap/css/bootstrap.min.cs') }}s">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('loginTemplate/fonts/font-awesome-4.7.0/css/font-awesome.min.cs') }}s">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('loginTemplate/fonts/Linearicons-Free-v1.0.0/icon-font.min.cs') }}s">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('loginTemplate/vendor/animate/animate.cs') }}s">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="{{ asset('loginTemplate/vendor/css-hamburgers/hamburgers.min.cs') }}s">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('loginTemplate/vendor/animsition/css/animsition.min.cs') }}s">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('loginTemplate/vendor/select2/select2.min.cs') }}s">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="{{ asset('loginTemplate/vendor/daterangepicker/daterangepicker.cs') }}s">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('loginTemplate/css/util.cs') }}s">
+	<link rel="stylesheet" type="text/css" href="{{ asset('loginTemplate/css/main.cs') }}s">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100 p-t-50 p-b-90">
+				<form form method="POST" action="{{ route('login') }}" class="login100-form validate-form flex-sb flex-w">
+                    @csrf
+                    <span class="login100-form-title p-b-51">
+						Login
+					</span>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+					
+					<div class="wrap-input100 validate-input m-b-16" data-validate = "Email is required">
+						<input class="input100 form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" id="email" type="email" name="email" placeholder="Email" value="{{ old('email') }}" required>
+                        <span class="focus-input100"></span>
+                        @if ($errors->has('email'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
+					</div>
+					
+					
+					<div class="wrap-input100 validate-input m-b-16" data-validate = "Password is required">
+						<input class="input100 form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" type="password" name="password" placeholder="Password" required> 
+                        <span class="focus-input100"></span>
+                        @if ($errors->has('password'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                        @endif
+					</div>
+					
+					<div class="flex-sb-m w-full p-t-3 p-b-24">
+						<div class="contact100-form-checkbox">
+							<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+							<label class="label-checkbox100" for="ckb1">
+								Remember me
+							</label>
+						</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+					</div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+					<div class="container-login100-form-btn m-t-17">
+						<button class="login100-form-btn" type="submit">
+                            {{ __('Login') }}
+						</button>
+					</div>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+<!--===============================================================================================-->
+	<script src="{{ asset('loginTemplate/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
+<!--===============================================================================================-->
+	<script src="{{ asset('loginTemplate/vendor/animsition/js/animsition.min.js') }}"></script>
+<!--===============================================================================================-->
+	<script src="{{ asset('loginTemplate/vendor/bootstrap/js/popper.js') }}"></script>
+	<script src="{{ asset('loginTemplate/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+<!--===============================================================================================-->
+	<script src="{{ asset('loginTemplate/vendor/select2/select2.min.js') }}"></script>
+<!--===============================================================================================-->
+	<script src="{{ asset('loginTemplate/vendor/daterangepicker/moment.min.js') }}"></script>
+	<script src="{{ asset('loginTemplate/vendor/daterangepicker/daterangepicker.js') }}"></script>
+<!--===============================================================================================-->
+	<script src="{{ asset('loginTemplate/vendor/countdowntime/countdowntime.js') }}"></script>
+<!--===============================================================================================-->
+	<script src="{{ asset('loginTemplate/js/main.js') }}"></script>
 
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+</body>
+</html>
